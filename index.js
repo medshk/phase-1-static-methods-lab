745aac6b586169dc4c8fc4659398dfddda3c97e0
med <mohammed.bennaceur98@gmail.com>
2023-10-09 12:43:45 +0100

    done

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1);
+  }
+  static sanitize(str) {
+    return str.replace(/[^a-zA-Z0-9\-\'\s]/g, "");
+  }
+  static titleize(str) {
+    const excludedWords = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    const words = str.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i].toLowerCase();
+      if (i === 0 || !excludedWords.includes(word)) {
+        words[i] = this.capitalize(word);
+      }
+    }
+
+    return words.join(" ");
+  }
 }
